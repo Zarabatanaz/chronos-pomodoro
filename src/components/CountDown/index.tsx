ebf8037994769e0d76b0b6c8c31aa585909e01e0
Carlos Wolf <carlosewolf@gmail.com>
2026-06-02 19:10:40 -0300

    created context to handle tasks states

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,5 +1,11 @@
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import styles from './styles.module.css';
 
+// type CountDownProps = HomeProps; // correto seria criar somente as propriedades para o countDown
+
 export function CountDown() {
-  return <div className={styles.container}>00:00:00</div>;
+  const { state } = useTaskContext();
+  return (
+    <div className={styles.container}>{state.formattedSecondsRemaining}</div>
+  );
 }
--- src/models/TaskStateModel.ts
+++ src/models/TaskStateModel.ts
@@ -1,14 +1,16 @@
 import type { TaskModel } from './TaskModels';
 
+// Estado -> Componente -> Filhos
+
 export type TaskStateModel = {
-  tasks: TaskModel[];
-  secondsRemaining: number;
-  formatedSecondsRemaining: string;
-  activeTask: TaskModel | null;
-  currentCycle: number; // 1 a 8
+  tasks: TaskModel[]; // histórico, MainForm
+  secondsRemaining: number; // Home, CountDown, histórico, MainForm, button
+  formattedSecondsRemaining: string; // Título, CountDown
+  activeTask: TaskModel | null; // CountDown, histórico, MainForm, button
+  currentCycle: number; // 1 a 8, Home
   config: {
-    workTime: number;
-    shortBreakTime: number;
-    longBreakTime: number;
+    workTime: number; // MainForm
+    shortBreakTime: number; // MainForm
+    longBreakTime: number; // MainForm
   };
 };
