1d81af025e2c0d938728e4aa27a058b6cd3ded86
Carlos Wolf <carlosewolf@gmail.com>
2026-06-02 15:46:41 -0300

    new components for generic html, mainForm and menu, Pages folder, new light theme, template folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,8 @@
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
-import { Heading } from './components/Heading';
-import { useState } from 'react';
+import { Home } from './pages/Home';
 
 export function App() {
-  // Lazy initialization
-  // const [numero, setNumero] = useState(() => {
-  //   console.log('Lazy initialization');
-  //   return 0;
-  // });
-
-  const [numero, setNumero] = useState(0);
-
-  function handleClick() {
-    setNumero(prevState => prevState + 1);
-  }
-
-  return (
-    <>
-      <Heading>Número: {numero}</Heading>
-      <button onClick={handleClick}>Aumenta</button>
-
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              id='meuInput'
-              type='text'
-              labelText='Tarefa'
-              placeholder='Digite a tarefa'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <button>Enviar</button>
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,35 @@
+import { DefaultInput } from '../DefaultInput';
+import { Cycles } from '../Cycles';
+import { DefaultButton } from '../DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
+
+export function MainForm() {
+  return (
+    <form className='form' action=''>
+      <div className='formRow'>
+        <DefaultInput
+          id='meuInput'
+          type='text'
+          labelText='Tarefa'
+          placeholder='Digite a tarefa'
+        />
+      </div>
+
+      <div className='formRow'>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+
+      <div className='formRow'>
+        <Cycles />
+      </div>
+
+      <div className='formRow'>
+        <button>Enviar</button>
+      </div>
+
+      <div className='formRow'>
+        <DefaultButton icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
+}
