64130760398091a5075ad1dc28af7f854095e8fa
Carlos Wolf <carlosewolf@gmail.com>
2026-06-03 19:37:43 -0300

    reducer to handle tasks, new tips component

---
+++ src/contexts/TaskContext/taskActions.ts
@@ -0,0 +1,38 @@
+// useReducer <- hook do React que recebe um reducer e um estado inicial
+// reducer <- função que recebe o estado atual e uma ação, e retorna o novo estado
+// state <- o estado atual
+// action <- a ação disparada, geralmente é um objeto com type e (opcionalmente) payload
+// type <- o tipo da ação, geralmente uma string (pode ser enum, constante, etc)
+// payload <- os dados extras enviados junto com a action, se necessário para atualizar o estado
+
+import type { TaskModel } from '../../models/TaskModels';
+
+// A maneira padrão da indústria para substituir enums é criar um objeto congelado e derivar o tipo a partir de suas chaves/valores
+
+// 1. Defina o objeto e trave-o com "as const" para torná-lo estático
+export const TaskActionTypes = {
+  START_TASK: 'START_TASK',
+  INTERRUPT_TASK: 'INTERRUPT_TASK',
+  RESET_STATE: 'RESET_STATE',
+} as const;
+
+// 2. Extraia o tipo dinamicamente a partir dos valores do objeto
+export type TaskActionTypes =
+  (typeof TaskActionTypes)[keyof typeof TaskActionTypes];
+
+export type TaskActionsWithPayload = {
+  type: typeof TaskActionTypes.START_TASK;
+  payload: TaskModel;
+};
+
+export type TaskActionsWithoutPayload =
+  | {
+      type: typeof TaskActionTypes.RESET_STATE;
+    }
+  | {
+      type: typeof TaskActionTypes.INTERRUPT_TASK;
+    };
+
+export type TaskActionModel =
+  | TaskActionsWithPayload
+  | TaskActionsWithoutPayload;
